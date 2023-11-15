9b71484f75aa1139079163c5d4ebdc2c6ec7b28b
clr <clrcera05@gmail.com>
2023-11-14 23:36:45 -0300

    what am i

--- Elevator/elevator.vhd
+++ Elevator/elevator.vhd
@@ -3,7 +3,9 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity elevator is
-    port (clk, reset : in bit;
+    port (
+          clk, clk_placa: std_logic;
+          reset: in bit;
           req: in bit;
           desired_floor: in unsigned(3 downto 0) := (others => '0');
           current: out unsigned(3 downto 0) := (others => '0');
@@ -16,16 +18,31 @@ architecture behaviour of elevator is
     signal state: states;
     signal current_floor: unsigned(3 downto 0) := (others => '0');
     signal objective_floor: unsigned(3 downto 0) := (others => '0');
+    signal fixed_clock: std_logic;
+
+    component Debouncing_Button_VHDL is
+        port(
+            button: in std_logic;
+            clk: in std_logic;
+            debounced_button: out std_logic
+        );
+    end component;
 
 begin
-    process (clk)
+    instance_debouncer: Debouncing_Button_VHDL
+        port map (
+            button => clk, clk => clk_placa,
+            debounced_button => fixed_clock
+        );
+
+    process (fixed_clock)
     begin
         if (reset = '1') then
             state <= not_moving;
             current_floor <= "0000";
             objective_floor <= "0000";
 
-        elsif (clk'event) and (clk = '1') then
+        elsif (fixed_clock'event) and (fixed_clock = '1') then
             case state is
 
                 when not_moving =>
@@ -42,23 +59,24 @@ begin
                 when rising =>
                     movement <= "01";
 
-                    if (objective_floor = current_floor) then
-                        state <= not_moving;
-                    else
                     current_floor <= current_floor + 1;
+                    current <= current_floor +1;
+                    
+                    if (objective_floor = current_floor+1) then
+                        state <= not_moving;
                     end if;
 
                 when descending =>
                     movement <= "10";
 
-                    if (objective_floor = current_floor) then
-                        state <= not_moving;
-                    else
                     current_floor <= current_floor - 1;
+                    current <= current_floor -1;
+
+                    if (objective_floor = current_floor-1) then
+                    state <= not_moving;
                     end if;
             
             end case;
         end if;
-        current <= current_floor;
     end process;
 end behaviour;
